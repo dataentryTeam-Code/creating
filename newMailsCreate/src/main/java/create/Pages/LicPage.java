19551d75e392c35aa45de3469ac00f40b516e510
RaviSasi <RaviSasi@DESKTOP-6HN1VBI>
2020-01-27 00:01:27 +0530

    Modified

--- newMailsCreate/src/main/java/create/Pages/LicPage.java
+++ newMailsCreate/src/main/java/create/Pages/LicPage.java
@@ -1,19 +1,23 @@
 package create.Pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 public class LicPage extends BaseTest
 {
-	public WebDriver driver;
 	public LicPage(WebDriver driver) 
 	{
 		this.driver=driver;
 		PageFactory.initElements(driver,this);
 	}
-
+	//Elements
+	public WebDriver driver;
+	@FindBy(xpath="//input[@value='Registered User']") WebElement registeredUser;
+	
+	//methods
 	public void clickOnRegisteredUser() 
 	{
-		driver.findElement(By.xpath("//input[@value='Registered User']")).click();
+		registeredUser.click();;
 	}
 }
