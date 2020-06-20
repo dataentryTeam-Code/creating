ce5db1a2115c0898c3edb80a31f0397cc2d3e31c
Msasi2726 <46027729+Msasi2726@users.noreply.github.com>
2020-06-20 22:00:51 +0530

    Modified.

--- newMailsCreate/src/main/java/create/inProgress/DadLic.java
+++ newMailsCreate/src/main/java/create/inProgress/DadLic.java
@@ -14,6 +14,7 @@ public class DadLic extends BaseTest
 	@Test
 	public void create() throws Exception
 	{	
+		//Dad lic script
 		WebDriver obj=startBrowser();
 		LicPage lic=new LicPage(obj);
 		lic.clickOnRegisteredUser();
